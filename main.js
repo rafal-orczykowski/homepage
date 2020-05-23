37a48c6d70d9ab1dcfaba3e7597e7a913bbb854d
Rafał Orczykowski <raf.orczykowski@gmail.com>
2020-05-23 17:08:22 +0200

    js css 5th week homework completed

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
-var name = prompt("jak masz na imię?");
-console.log(`cześć ${name}, miło Cię widzieć`);
+var nameIn = prompt("jak masz na imię?");
+console.log(`cześć ${nameIn}, miło Cię widzieć`);
 
-const heading = document.querySelector('.header__tittle--js')
-heading.innerHTML = `Cześć ${name}, miło Cię widzieć!`
-
+const heading = document.querySelector(".header__tittle--js");
+heading.innerHTML = `Cześć ${nameIn}, miło Cię widzieć!`;
+const name = "Rafał";
 const age = 31;
 
 const showMyProperty = (myProperty) => {
-    console.log(`mój wiek to ${myProperty}`);
-    myProperty= myProperty*2;
+  console.log(`mój wiek to ${myProperty}`);
+  myProperty = myProperty * 2
+  return myProperty;
+};
+const year = showMyProperty(age);
 
- 
-}
-const Year = showMyProperty(age)
+showMyProperty(year);
 
-showMyProperty(Year);
-
-function secondProperty(sProperty){
-console.log(`mój wiek to ${sProperty}`);
-sProperty= sProperty*2;
-return sProperty;
+function secondProperty(sProperty) {
+  console.log(`mój wiek to ${sProperty}`);
+  sProperty = sProperty * 5;
+  return sProperty;
 }
 const myAge = secondProperty(age);
 
 secondProperty(age);
 secondProperty(myAge);
+
+
+function greeeting(querySelectorContent,content){
+    const element = document.querySelector(querySelectorContent);
+    element.innerHTML = content;
+}
+
+greeeting('.header__tittle--js', "witaj ")
+
+
+const nextGreeting = (name,age) => {
+    console.log(`Cześć mam na imię ${name} i mam ${age}lat`);
+}
+
+nextGreeting("Paula",30)
+
